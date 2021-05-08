296aea903e930350676d422083bf092347bf560e
Mickdevil <trxctvolka@gmail.com>
2021-05-08 17:42:14 +0200

    Finlaly i find the solution, I used https://www.freemaptools.com/find-places-within-radius.htm to get places, then in other project i will implement this source to main parser

--- app/src/main/java/com/mickdevil/parsegooglewithblackjackandhookers/MainActivity.java
+++ app/src/main/java/com/mickdevil/parsegooglewithblackjackandhookers/MainActivity.java
@@ -10,13 +10,23 @@ import android.view.View;
 import android.webkit.CookieManager;
 import android.widget.Button;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -32,13 +42,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         parse = findViewById(R.id.parse);
-Thread thread = new Thread(new secT());
+        Thread thread = new Thread(new secT());
 
 
         parse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!thread.isAlive()){
+                if (!thread.isAlive()) {
                     thread.start();
                     Log.d(TAG, "onCreate: " + "it start the sec thread!!!");
                 }
@@ -48,48 +58,51 @@ Thread thread = new Thread(new secT());
     }
 
 
-private void getWeb(){
-    try {
+    private void getWeb() {
 
+        String webAdress = "https://api.foursquare.com/v2/venues/search?ll=47.2122%2C-1.5450&radius=5000&categoryId" +
+                "=4d4b7105d754a06374d81259&client_id=MDW1CQIF4AUNLX2VXX2FJ1CA4CZIWQ3ACUQTCK0JNQ2MXVRO&client_secret=YI1SJ" +
+                "4Q4CKJZV3NK0YPGCCLIYZTRLCNTW3MDBHPBRJXMUUCL&v=20200101";
+        String line = "";
+        String data = "";
+        JSONObject Johny = null;
 
+        try {
+            URL url = new URL(webAdress);
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            //  urlConnection.setRequestMethod("GET");
+            InputStream inputStream = urlConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
-        // doc = Jsoup.connect("https://www.google.fr/maps/search/kebab/@47.4793553,-0.5454856,13z/data=!4m2!2m1!6e5").get();
-        doc = Jsoup.connect("https://www.google.fr/maps/search/kebab/@47.4793553,-0.5454856,13z/data=!4m2!2m1!6e5").get();
+            while (line != null) {
 
-
-        doc = Jsoup.connect("https://www.google.ru/maps/place/Ashiq+Muhammad/data=!4m5!3m4!1s0x480878ead1b59d01:0x872d" +
-                "fd9e9c1f2bd7!8m2!3d47.4677682!4d-0.5505496?authuser=0&amp;hl=ru&amp;rclk=1\" class=\"place-result-container-place" +
-                "-link\" jsan=\"7.place-result-container-place-link,0.aria-label,8.href,0.jsaction").get();
-
-        Log.d(TAG, "getWeb: " + doc.text());
-
-      //  getCookie()
-
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-
-}
-
-
-
-    public String getCookie(String siteName,String CookieName){
-        String CookieValue = null;
-
-        CookieManager cookieManager = CookieManager.getInstance();
-        String cookies = cookieManager.getCookie(siteName);
-        if(cookies != null){
-            String[] temp=cookies.split(";");
-            for (String ar1 : temp ){
-                if(ar1.contains(CookieName)){
-                    String[] temp1=ar1.split("=");
-                    CookieValue = temp1[1];
-                }
+                line = bufferedReader.readLine();
+                data += line;
             }
+
+            Johny = new JSONObject(data);
+
+              Log.d(TAG, "getPlaces: " + data);
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-        return CookieValue;
+
+
+    }
+
+
+    public class secT implements Runnable {
+
+        @Override
+        public void run() {
+            getWeb();
+        }
     }
 
 
@@ -98,13 +111,10 @@ private void getWeb(){
 
 
 
-public class secT implements Runnable{
 
-    @Override
-    public void run() {
-        getWeb();
-    }
-}
+
+
+
 
 
 
